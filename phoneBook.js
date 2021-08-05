ea3eef13c27587d0965c759c1faf6f3e5c6d9a56
MohaimenH <mohaimenhassan08@gmail.com>
2021-08-05 20:50:19 +0600

    Minor tweak on Morgan middleware

--- phoneBook.js
+++ phoneBook.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 morgan.token("content", (request) => {
-    return JSON.stringify(request.body);
+    return request.method === "POST" ? JSON.stringify(request.body) : " ";
 });
 
 app.use(
